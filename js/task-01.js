1728e07df8b7d8232b478648a997c4546a0bf041
Victor8825 <victorrv2588@gmail.com>
2022-05-19 14:46:29 +0300

    1 task optimised

--- js/task-01.js
+++ js/task-01.js
@@ -1,23 +1,7 @@
-const bodyEl = document.body;
+const categoriesInfoEl = document.querySelectorAll('.item');
+console.log(`Number of categories: ${categoriesInfoEl.length}`);
 
-const categoriesListEl = bodyEl.querySelector('#categories');
-
-const categoriesItemsEl = categoriesListEl.children;
-console.log(`Number of categories: ${categoriesItemsEl.length}`);
-
-const categoriesItemsListEl = bodyEl.querySelectorAll('.item > ul');
-
-const categoryTitlesEl = bodyEl.querySelectorAll('.item');
-categoryTitlesEl.forEach(elem => {
+categoriesInfoEl.forEach(elem => {
   console.log(`Category: ${elem.firstElementChild.textContent}`)
   console.log(`Elements: ${elem.querySelector('ul').children.length}`)
-  // categoriesItemsListEl.forEach(elem => {
-  //   console.log(`Elements: ${elem.children.length}`)
-  // })
-})
-
-
-// console.log(categoriesItemsListEl);
-
-
-
+  });
