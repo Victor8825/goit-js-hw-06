54b9dad0f29ab321e25ae36cd2cec6423a655808
Victor8825 <victorrv2588@gmail.com>
2022-05-19 17:12:22 +0300

    task 2 completed

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,17 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsNewArray = ingredients.map(elem => {
+const ingridientsItemEl = document.createElement('li');
+ingridientsItemEl.textContent = elem;
+ingridientsItemEl.classList.add('item');
+
+return ingridientsItemEl;
+});
+
+const ingredientsListEl = document.querySelector('#ingredients');
+ingredientsListEl.append(...ingredientsNewArray);
+
+console.log(ingredientsListEl);
+
