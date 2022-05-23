a3b4fa0dc7db094873146b1bd6c1b95473998bcc
Victor8825 <victorrv2588@gmail.com>
2022-05-23 16:20:45 +0300

    task 6 finished at all

--- js/task-06.js
+++ js/task-06.js
@@ -1,7 +1,6 @@
 const inputEl = document.querySelector("#validation-input");
 
 function onInputBlur () {
-  console.log(this.dataset);
   if ( this.dataset.length == this.value.length ) {
     this.classList.remove("invalid");
     this.classList.add( "valid" );
@@ -10,7 +9,6 @@ function onInputBlur () {
     this.classList.remove("valid");
     this.classList.add( "invalid" );
   }
-  // inputEl.dataset.length == inputEl.value.length ? inputEl.classList.add("valid") : inputEl.classList.add("invalid");
 }
 
 inputEl.addEventListener("blur", onInputBlur);
