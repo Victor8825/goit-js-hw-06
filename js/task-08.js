5d6b8a4c2b93e2f71c6af20bb7b1937ce6befc17
Victor8825 <victorrv2588@gmail.com>
2022-05-25 18:53:46 +0300

    task 8 completed

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,23 @@
+const formEl = document.querySelector(".login-form");
+
+function onFormSubmit (event) {
+  event.preventDefault();
+  const elements = event.currentTarget.elements;
+  const email = elements.email.value;
+  const password = elements.password.value;
+
+  if( email === "" || password === "" ) {
+    alert("Все поля формы должны быть заполнены");
+  }
+  else {
+    const formData = {
+      email,
+      password
+    };
+    console.log(formData);
+    formEl.reset();
+  }
+}
+
+formEl.addEventListener("submit", onFormSubmit);
 
