5b8cc6670c7717d3e4f082849a122d29bd71f4bd
Victor8825 <victorrv2588@gmail.com>
2022-05-26 07:33:23 +0300

    task 9 completed

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+function changeBodyColor () {
+  bodyEl.style.backgroundColor = getRandomHexColor();
+  bodyElColorTitle.textContent = bodyEl.style.backgroundColor;
+}
+
+const bodyEl = document.body;
+const buttonEl = document.querySelector(".change-color");
+const bodyElColorTitle = document.querySelector("span.color");
+
+buttonEl.addEventListener("click", changeBodyColor);
