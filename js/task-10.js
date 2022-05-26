84c2a60d4c8575b5a63c8bc2ea4e2b94c05127a6
Victor8825 <victorrv2588@gmail.com>
2022-05-26 16:08:05 +0300

    task 10 completed

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,29 @@
+const controlsEl = document.querySelector("#controls");
+const inputEl = controlsEl.firstElementChild;
+const buttonCreate = controlsEl.querySelector("[data-create]");
+const buttonDestroy = controlsEl.lastElementChild;
+const boxesWrap = document.querySelector("#boxes");
+const initialHeight = 30;
+const initialWidth = 30;
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+function createBoxes(amount) {
+    for( let i = 0; i < amount; i += 1) {
+    const boxesItem = document.createElement("div")
+    boxesItem.style.width = initialWidth + 10 * i + "px";
+    boxesItem.style.height = initialHeight + 10 * i + "px";
+    boxesItem.style.backgroundColor = getRandomHexColor();
+    boxesWrap.append(boxesItem);
+    console.log(boxesItem);
+  }
+}
+
+function destroyBoxes() {
+  boxesWrap.innerHTML = "";
+}
+
+buttonCreate.addEventListener( "click", () => {createBoxes(inputEl.value)} );
+buttonDestroy.addEventListener("click", destroyBoxes);
